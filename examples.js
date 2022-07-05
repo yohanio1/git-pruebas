d2910b935d8c78e6134e38d8cdd646ce32f1f3e4
JuanAcevedo <juanest31@hotmail.com>
2022-07-04 23:21:11 -0500

    Solucion primer ejercicio JavaScript

--- examples.js
+++ examples.js
@@ -1,1 +1,28 @@
-console.log(`Hola Mundo`)
+/*1)Programa una función que cuente el 
+número de caracteres de una cadena de 
+texto.
+*/
+
+function numCaracteres(cadena){
+
+    switch(typeof(cadena)){
+        case 'string': 
+            console.log(cadena.length);
+            break;
+        case 'number':
+            console.log("Se convierte el número");
+            console.log(cadena.toString().length);
+            break;
+        default:
+            console.error(`Error el tipo de dato debe ser string y tu tipo de dato es ${typeof(cadena)}`);
+    }
+}
+
+const Persona = {
+    nombre: "Juan",
+    edad: 12
+};
+
+let a = 23;
+
+numCaracteres(Persona);
