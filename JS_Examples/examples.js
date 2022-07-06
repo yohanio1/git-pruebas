efda8df41dad30ab36003944c1c7bbf36546ad59
JuanAcevedo <juanest31@hotmail.com>
2022-07-06 12:39:21 -0500

    Añadido prueba y solución del ejercicio 2

--- JS_Examples/examples.js
+++ JS_Examples/examples.js
@@ -3,8 +3,24 @@ número de caracteres de una cadena de
 texto.
 */
 
-function numCadena(cadena = "") {
+const numCadena = (cadena = "") =>{
     return cadena.length;
   }
+  
 
-  module.exports = numCadena;
+
+/*2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, 
+pe. miFuncion("Hola Mundo", 4) devolverá "Hola".*/
+
+const recortar = (cadena,numCaracteres) =>{
+  if (typeof(cadena) === 'object'){
+    return undefined;
+  }else{
+  return cadena.slice(0,numCaracteres);
+  }
+}
+
+
+//Exportación de Módulos
+
+module.exports = {numCadena, recortar};
--- JS_Examples/unit_test/index.test.js
+++ JS_Examples/unit_test/index.test.js
@@ -1,12 +1,40 @@
-const numCadena = require('../examples');
+const excercise = require('../examples');
+//const recortar = require('../examples');
 
-describe('cadenas test',() =>{
+
+describe('excercise #1',() =>{
 
   test('cadena Hola length =  4', () => {
-    expect(numCadena("Hola")).toBe(4);
+    expect(excercise.numCadena("Hola")).toBe(4);
   });
   test('number is undefined',() =>{
-    expect(numCadena(12)).toBeUndefined();
+    expect(excercise.numCadena(12)).toBeUndefined();
   });
+  test('object is undefined',() =>{
+    let objectTest = {
+      nombre: "Juan",
+      edad: 12
+    };
+    expect(excercise.numCadena(objectTest)).toBeUndefined();
+  });
+})
 
+describe('excercise #2',() =>{
+
+  test('slice = 4 Hola Mundo => result Hola ', () => {
+    expect(excercise.recortar("Hola Mundo",4)).toBe("Hola");
+  });
+  test('slice = 3 Esto es una prueba => result Est ', () => {
+    expect(excercise.recortar("Esto es una prueba",3)).toBe("Est");
+  });
+  test('slice = n empty string => result empty string ', () => {
+    expect(excercise.recortar("",3)).toBe("");
+  });
+  test('slice = n object => result undefined ', () => {
+    let objectTest = {
+      nombre: "Juan",
+      edad: 12
+    };
+    expect(excercise.recortar(objectTest,3)).toBeUndefined();
+  });
 })
